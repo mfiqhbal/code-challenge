3519ab2fd995e418465a28924c5e983ec7ad60bd
Iqhbal <mf.iqhbal@gmail.com>
2024-09-25 23:59:49 +0800

    fix import function

--- server/app/Imports/StudentsImport.php
+++ server/app/Imports/StudentsImport.php
@@ -4,9 +4,16 @@ namespace App\Imports;
 
 use App\Models\Student;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class StudentsImport implements ToModel
+class StudentsImport implements ToModel, WithStartRow
 {
+
+    public function startRow(): int
+    {
+        return 2;
+    }
+
     public function model(array $row)
     {
         // Skip the row if the name field is empty or null
